14167348b38606e7dbff6d83e7c5141a559a046c
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-04-15 12:48:32 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 11:48:47 2024
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+st.title("BASE DE DATOS NACIONAL DE PENSIONES NO RECLAMADAS")
+
+st.header("Debe tener a la mano su CURP y su NÚMERO DE SEGURIDAD SOCIAL (NSS)")
+
+st.subheader("")
+st.subheader("")
+st.subheader("")
+
+
+# Create an empty container
+placeholder = st.empty()
+
+actual_email = "administrador"
+actual_password = "general123"
+
+# Insert a form in the container
+with placeholder.form("Acceder"):
+    st.markdown("#### Intoducir:")
+    email = st.text_input("CURP")
+    password = st.text_input("NSS", type="password")
+    submit = st.form_submit_button("Login")
+
+if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    st.success("CURP y NSS CORRECTOS")
+    
+    link = '[Puede continuar:](https://000000001-ivhfhbeaaxnnbchxfmg3hb.streamlit.app/)'
+    st.markdown(link, unsafe_allow_html=True)
+    
+    
+elif submit and email != actual_email and password != actual_password:
+    st.error("DATOS INCORRECTOS")
+else:
+    pass
